013da80bf17008ed5330b26725c99286abdea3e2
Rami Essamedeen <rami3sam@Rami.>
2026-02-22 13:49:00 +0400

    added fee subtraction logic

--- fx-quote-service/src/controllers/quotes.ts
+++ fx-quote-service/src/controllers/quotes.ts
@@ -5,17 +5,25 @@ import { getFXRate } from "../utils/provideFakeConversion.ts";
 import type { CurrencyCodes } from "../enums/currencyCodes.enum.ts";
 
 const generateQuote = (req: Request, res: Response) => {
-    try {
-        const fxInfo = generateQuoteSchema.parse(req.body);
-        const fxRate: number = getFXRate(fxInfo.sendCurrency as CurrencyCodes, fxInfo.payoutCurrency as CurrencyCodes)
-        const feePercentage : number = 0.05
-        const feeAmount: currency = currency(fxInfo.sendAmount).multiply(feePercentage)
-        const payoutAmount : currency = currency(fxInfo.sendAmount).multiply(fxRate)
-        const quoteExpiry = new Date(Date.now() + 60 * 1000);
-        res.status(200).json({fxRate,feeAmount, payoutAmount, quoteExpiry })
-    } catch (err: any) {
-        res.status(400).json(err.message);
-    }
-}
+  try {
+    const fxInfo = generateQuoteSchema.parse(req.body);
+    const fxRate: number = getFXRate(
+      fxInfo.sendCurrency as CurrencyCodes,
+      fxInfo.payoutCurrency as CurrencyCodes,
+    );
+    const feePercentage: number = 0.025;
+    const feeAmount: currency = currency(fxInfo.sendAmount).multiply(
+      feePercentage,
+    );
+    const payoutAmount: currency = currency(fxInfo.sendAmount)
+      .subtract(feeAmount)
+      .multiply(fxRate);
+      
+    const quoteExpiry = new Date(Date.now() + 60 * 1000);
+    res.status(200).json({ fxRate, feeAmount, payoutAmount, quoteExpiry });
+  } catch (err: any) {
+    res.status(400).json(err.message);
+  }
+};
 
-export { generateQuote }
+export { generateQuote };
